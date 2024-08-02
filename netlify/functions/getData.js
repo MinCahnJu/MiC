fd5fd12c186c972c9537a7dd86e5b22e6a27cf10
MinCahnJu <mcj00220077@gmail.com>
2024-08-03 06:30:27 +0900

    데이터베이스

---
+++ netlify/functions/getData.js
@@ -0,0 +1,25 @@
+// netlify/functions/getData.js
+require('dotenv').config();
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+exports.handler = async function(event, context) {
+  const { data, error } = await supabase
+    .from('contests')
+    .select('*');
+
+  if (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
