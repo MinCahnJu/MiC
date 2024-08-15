74c482b80ccef2f33d43f3c934d214a06632c4b6
MinChanJu <mcj00220077@gmail.com>
2024-08-16 02:49:21 +0900

    로그인 페이지 변경

--- resources/static/JS/user.js
+++ resources/static/JS/user.js
@@ -10,8 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
     output += '</div>'
     document.getElementById("user").innerHTML = output;
   } else {
-    output += `<a href="/register">회원가입</a>`;
-    output += `<span class="login-devider"></span>`;
     output += `<a href="/login">로그인</a>`;
     output += '</div>'
     document.getElementById("user").innerHTML = output;
